a9fcab1731478a45c1c446fdb1e83a568a6bba18
Anto-LSR <anton1.lesueur@gmail.com>
2021-07-08 15:19:46 +0200

    Version click-flip-card. 08/07/21

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+(function () {
+  window.onload = function () {
+    $(".flip-card .flip-card-inner").click(function () {
+    $(this).closest(".flip-card").toggleClass("hover");
+      $(this).css("transform, rotateY(180deg)");
+    });
+  };
+})();
